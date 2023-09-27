85ba526f05f58a81966c5d1134a950d1f2517f89
Harthann <nca.eyraud@gmail.com>
2023-09-27 15:21:11 +0200

    adding header line

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,13 @@ mod interpretor;
 fn main() {
     let config = parse::parse_machine();
     let input = ".".to_string();
-    let input = "11111111-1=".to_string();
+    let input = "11111111+1=".to_string();
     match config {
         Ok(machine) => {
             println!("{}\n{:=<80}", machine, "");
             let mut emulator = interpretor::TuringInterpret::new(machine, input.clone());
             println!("[{0:^padding$}]", input, padding=78);
+            println!("{:=<80}", "");
             emulator.run();
         },
         Err(err) => eprintln!("{}", err)
